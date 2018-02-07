9bed2a82c97f90291c820a7c6674bd487eddd428
Cris Shaker <iamcrisshaker@gmail.com>
2018-02-07 15:23:37 +0000

    added admin authentication pages

--- router.js
+++ router.js
@@ -2,4 +2,12 @@ module.exports = (app) => {
   app.get("/", (req, res) => {
     res.render("index");
   });
+
+  app.get("/admin/login", (req, res) => {
+    res.render("admin/login");
+  });
+
+  app.get("/admin/register", (req, res) => {
+    res.render("admin/register");
+  });
 }
